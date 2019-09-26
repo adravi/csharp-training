a46e03eec1e4928688da658e7e5dd4214c15290e
Salvador Avila <saavilae@microsoft.com>
2019-09-25 17:51:43 -0700

    Homework for session 1.

---
+++ src/Session2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Session2
+{
+    class Program
+    {
+        /*
+         * 
+         * Start a cycle.
+         * Generate a random number between 1 and 100
+         * Save the first time ever-generated number into a global variable named x         
+         * Call method DisplaySequence passing the <number> as parameter
+            
+          Inside method:  
+            Print a character a <number> amount of times
+            If <number> is even, print '*'
+            If <number> is odd, print '+'
+            Print until the number received is equals to exactly (2x + 1)
+         * 
+         */
+        private static int firstNumber;
+        static void Main(string[] args)
+        {
+            var randomNumber = 10; // TODO: Change it to random           
+            // Put this call inside a cycle
+            DisplaySequence(randomNumber);
+        }
+        private static void DisplaySequence(int number)
+        {
+
+        }
+    }
+}
+}
