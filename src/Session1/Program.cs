ad641b8a19c01ad5eaa7dfcebd4eb28b1f4c9c0e
Salvador Avila <saavilae@microsoft.com>
2019-09-25 17:22:01 -0700

    Changes after sesion 1.

---
+++ src/Session1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Session1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Explicit Typing
+            // Declaration
+            int number;
+            char[] letters2 = new char[5];
+
+            // Implicit Typing
+            // Declaration + Initialization
+            var number2 = 2;
+            var letters = new char[5];
+
+            // Initial Values in Array are null
+            var words = new string[10];
+            var blank = string.Empty;
+            Console.WriteLine(words[0]);
+            Console.WriteLine(words[0] == null);
+            Console.WriteLine("Hello World!");
+
+            // Implicit Declaration and Initialization
+            // Array's first index is big fat ZERO 
+            // Last one = Size - 1
+            var namedWords = new [] { "zero", "one" };
+            Console.WriteLine(namedWords[1]);
+
+
+        }
+    }
+}
