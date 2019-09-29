6e95c6838c31d66e1a27f72fec6b8ac32171fe1b
Sal Avila <sal.avila@outlook.com>
2019-09-29 14:29:12 -0700

    Meaningless changes. Solution was correct, Pragyan!

--- src/Session2/Program.cs
+++ src/Session2/Program.cs
@@ -9,7 +9,7 @@ namespace Session2
         /*
          * 
          * Start a cycle.
-         * Generate a random number between 1 and 100
+         * Generate a random number between 1 and 20
          * Save the first time ever-generated number into a global variable named x         
          * Call method DisplaySequence passing the <number> as parameter
             
@@ -17,7 +17,7 @@ namespace Session2
             Print a character a <number> amount of times
             If <number> is even, print '*'
             If <number> is odd, print '+'
-            Print until the number received is equals to exactly (2x + 1)
+            Print until the number received is equals to exactly : 12
          * 
          */
                  
@@ -30,24 +30,26 @@ namespace Session2
 
             do
             {
-                randomNumber = randomGenerator.Next(1, 10);
+                randomNumber = randomGenerator.Next(1, 20);
 
                 if (firstNumber == 0)
                 {
                     firstNumber = randomNumber;
                 }
-                Console.Write("\n" + randomNumber + " : ");
+                
                 DisplaySequence(randomNumber);
 
-            } while (randomNumber != (8));
+            } while (randomNumber != 12);
             
         }
                 
         private static void DisplaySequence(int number)
-        {            
+        {
+            Console.Write("\n" + number + " : ");
+
             for (int i = 0; i < number; i++)
             {
-                if(number % 2 ==0)
+                if(number % 2 == 0)
                 {
                     Console.Write("*");
                 }
