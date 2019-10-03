a0a47ef9f9906368a5446aae85f2663425cbc167
Salvador Avila <saavilae@microsoft.com>
2019-10-02 19:38:33 -0700

    TODO modifications. Partial solution.

--- src/Session3/RomanParser.cs
+++ src/Session3/RomanParser.cs
@@ -6,7 +6,51 @@ namespace Session3
     {
         public int RomanToInt(string romanNumber)
         {
-            return -1;
+            // Get the value of first symbol
+            int firstSymbol; // TODO: Make the appropriate calculation!
+
+            // First value
+            int sum = -1; // TODO: Initialize sum with the right value!
+
+            // The first previous is first symbol ever
+            int prev; // TODO: Initialize the value of prev!
+
+            // Start iterate the roman number, from the second symbol
+            // TODO: Start the value of i on the appropriate value!
+            for (int i = -1; i < romanNumber.Length; i++)
+            {
+                char symbol = romanNumber[i];
+                sum += EvalSymbol('z', 'z'); // TODO: Make the call with the appropriate values!
+            }
+
+            return sum;
+        }
+
+        private int EvalSymbol(char prev, char curr)
+        {
+            var val = 0;
+
+            // TODO: Build the switch case statement
+            //switch (curr)
+            //{
+            //    case 'I':
+            //        // Action
+            //        if (prev.Equals('z'))
+            //        {
+                        // TODO: Update sum accordingly
+                        // sum += -1;
+            //        }
+            //        else
+            //        {
+
+            //        }
+            //        break;
+            //    // ... // TODO: Finish the cases
+            //    default:
+            //        break;
+            //}
+
+            return val;
         }
     }
 }
