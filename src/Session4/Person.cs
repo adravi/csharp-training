f61365006b6220e117fbb1f5ed4209b9b7e72cd7
Salvador Avila <saavilae@microsoft.com>
2019-10-15 17:04:51 -0700

    Session 4. Classes and Objects.

---
+++ src/Session4/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace Session4
+{
+    public class Person
+    {
+        // Basic Constructor
+        public Person() {}
+
+        // Custom Constructor
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        // Properties
+        public string Name { get; set; }
+        
+        public int Age { get; set; }
+        
+        public string Address { get; set; }
+
+        // Fields
+        private int PhoneNumber;
+
+        // Methods
+        public void Eat() {}
+
+        public int GetRetirementYear()
+        {
+            return CalculateRetirementYear();
+        }
+
+        private int CalculateRetirementYear()
+        {
+            return -1;
+        }
+    }
+}
---
+++ src/Session4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Session4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person personObject1 = new Person();
+
+            personObject1.Name = "Aaron";
+            personObject1.Address = "1438 Ft Street";
+            personObject1.Age = 50;
+            
+            Console.WriteLine("Program starts here");
+            Console.WriteLine(personObject1.Name);
+            Console.WriteLine(personObject1.Age);
+
+            Person personObject2 = new Person();
+            personObject2.Name = "Pragyan";
+            personObject2.Address = "1438 Ft Street";
+
+            Person personObject3 = new Person("Chintan");
+        }
+    }
+}
