81418e6cfcba31366f15b5f1ec11bd1182ee11bf
Pragyan Paudel <prpaudel@microsoft.com>
2019-10-23 16:57:27 -0700

    Session 5 changes

---
+++ src/Session5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Session5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square();
+            square.Length = 20;
+            square.Color = "Blue";
+            Console.WriteLine("Length: " + square.Length);
+            Console.WriteLine("Area: " + square.Area);
+            Console.WriteLine("Volume: " + square.Volume);
+            Console.WriteLine("Color: " + square.Color);
+        }
+    }
+}
---
+++ src/Session5/Square.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Session5
+{
+    public class Square
+    {
+        public int Length { get; set; }
+
+        public int Area
+        {
+            get
+            {
+                return Length * Length;
+            }
+        }
+
+        public int Volume
+        {
+            get
+            {
+                // Assume a cube
+                return Area * Length;
+            }
+        }
+        public string Color { get; set; }
+    }
+}
